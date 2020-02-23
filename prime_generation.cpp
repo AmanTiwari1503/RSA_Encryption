233dbcbb12671896f0b938f745153787b916559f
AmanTiwari1503 <tiwariaman2000@gmail.com>
2020-02-24 03:02:14 +0530

    Added necessary corrections

--- prime_generation.cpp
+++ prime_generation.cpp
@@ -4,27 +4,47 @@
 #include <time.h>
 #include <iostream>
 #include <unistd.h>
+#include <tuple>
+#include <bits/stdc++.h>
 using namespace std;
 
+void generate_keys(mpz_t e, mpz_t d, mpz_t n);
 void generate_prime(mpz_t prime_value, int bits);
-int generate_keys(mpz_t d, mpz_t e, mpz_t n);
 void generate_random_number(mpz_t r_number, int size);
-int main(){
-	mpz_t p,q,n,one,e,phi_n,d;
-	mpz_inits(p,q,n,phi_n,e,d,NULL);
+void build_dictionary(mpz_t dict[][3], int c_size);
+
+void build_dictionary(mpz_t dict[][3], int c_size){
+	mpz_t e,d,n;
+	for(int i=0;i<c_size;i++)
+	{
+		mpz_inits(dict[i][0],dict[i][1],dict[i][2], NULL);
+		generate_keys(dict[i][0],dict[i][1],dict[i][2]);
+		printf("Company %d\n",(i+1));
+		gmp_printf("Public Keys:\ne:%Zd\nn:%Zd\n",dict[i][0], dict[i][2]);
+		gmp_printf("Private Key:\nd:%Zd\n",dict[i][1]);
+	}
+}
+
+void generate_keys(mpz_t e, mpz_t d, mpz_t n){
+	mpz_t p,q,one,phi_n;
+	mpz_inits(p,q,phi_n,NULL);
 	mpz_init_set_ui(one,1);
 	generate_prime(p,256);
-	usleep(5000000);
+	usleep(3000000);
 	generate_prime(q,256);
-	gmp_printf("p:%Zd\nq:%Zd\n",p,q);
 	mpz_mul(n,p,q);
 	mpz_sub(p,p,one);
 	mpz_sub(q,q,one);
 	mpz_mul(phi_n,p,q);
-	generate_keys(d,e,phi_n);
-	gmp_printf("Public Keys:\ne:%Zd\nn:%Zd\n",e,n);
-	gmp_printf("Private Key:\nd:%Zd\n",d);
-	mpz_clears(p,q,n,one,phi_n,e,d,NULL);
+	while(true){
+		generate_random_number(e,500);
+		int p = mpz_invert(d,e,phi_n);
+		if(p==0)
+			continue;
+		else
+			break;
+	}
+	mpz_clears(p,q,one,phi_n,NULL);
 }
 
 void generate_prime(mpz_t prime_value, int bits){
@@ -43,17 +63,6 @@ void generate_prime(mpz_t prime_value, int bits){
 			continue;
 	}
 	gmp_randclear(state);
-}
-int generate_keys(mpz_t d, mpz_t e, mpz_t n){
-	while(true){
-		generate_random_number(e,500);
-		int p = mpz_invert(d,e,n);
-		if(p==0)
-			continue;
-		else
-			break;
-	}
-	
 }
 void generate_random_number(mpz_t r_number, int size){
 	gmp_randstate_t state;
