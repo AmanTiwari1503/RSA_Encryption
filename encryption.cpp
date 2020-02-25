7412eda8ee474590765a1e92683d6e55bc80a7c7
AmanTiwari1503 <tiwariaman2000@gmail.com>
2020-02-25 12:46:14 +0530

    Completed block encryption

--- encryption.cpp
+++ encryption.cpp
@@ -1,16 +1,36 @@
 #include <gmp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
+#include <bits/stdc++.h>
 #include "prime_generation.cpp"
 
 int determine_block_size(int alpha_set, mpz_t n);
+string encrypt_block(string block,mpz_t e,mpz_t n);
+
 int main(){
-	mpz_t pr;
-	mpz_init(pr);
-	generate_random_number(pr,512);
-	int k = determine_block_size(29,pr);
+	mpz_t e,d,n;
+	mpz_inits(e,d,n,NULL);
+	generate_keys(e,d,n);
+	int k = determine_block_size(29,n);
 	// gmp_printf("%Zd\n",pr);
 	// printf("Block Size :%d\n", k);
+	string m,tmp = "";int count = 0;
+	printf("Enter the message to be encrypted\n");
+	getline(cin,m);
+	transform(m.begin(), m.end(), m.begin(), ::toupper); 
+	for(int i=0;i<m.length();i++)
+	{
+		count = count+1;
+		tmp += m.at(i);
+		if(count>=k)
+		{
+			tmp = encrypt_block(tmp,e,n);
+			cout<<tmp<<"\n";
+			count = 0;
+			tmp="";
+		}
+	}
 }
 
 int determine_block_size(int alpha_set, mpz_t n){
@@ -26,4 +46,40 @@ int determine_block_size(int alpha_set, mpz_t n){
 		p = p+1;
 	}
 	return p-1;
+}
+
+string encrypt_block(string block,mpz_t e,mpz_t n){
+	mpz_t m,c,temp;
+	mpz_inits(m, c, temp, NULL);
+	int b_size = block.length();
+	for(int i=0;i<block.length();i++)
+	{
+		b_size = b_size-1;
+		char ch = block.at(i);
+		int c_n = 0;
+		if(ch == '.')
+			c_n = 26;
+		else if(ch == ',')
+			c_n = 27;
+		else if(ch == '.')
+			c_n = 28;
+		else
+			c_n = (int)ch - 97;
+
+		mpz_ui_pow_ui(temp,29,b_size);
+		mpz_addmul_ui(m,temp,c_n);
+	}
+	mpz_powm(c,m,e,n);
+	mpz_clear(m);
+	mpz_t q;
+	mpz_init(q);
+	string encypt = "";
+	for(int i=block.length();i>=0;i--)
+	{
+		mpz_ui_pow_ui(temp,29,i);
+		mpz_tdiv_qr(q,c,c,temp);
+		encypt += (char)(mpz_get_ui(q)+97);
+	}
+	mpz_clears(c, q, temp, NULL);
+	return encypt;
 }
